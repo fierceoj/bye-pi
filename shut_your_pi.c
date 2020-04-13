c632225b167deb53f0f15949bf467a5cfe041a1d
fierceoj <61847904+fierceoj@users.noreply.github.com>
2020-04-13 17:29:28 -0500

    Create shut_your_pi.c

---
+++ shut_your_pi.c
@@ -0,0 +1,25 @@
+//C code to shut down the raspberry pi when a button is pressed
+//uses wiringPi library and wiringPi pin numbers
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+
+int main(void) {
+        wiringPiSetup();
+        
+        // wiringPi pin 29, physical pin 40, BCM pin 21 
+        const int button = 29;
+
+        //set pin 29 as input
+        pinMode(button, INPUT);
+
+        //wait for button press
+        while (1) {
+                if (digitalRead(button) == HIGH) {
+                        printf("Powering down the raspberry pi...\n");
+                        system("sudo poweroff");
+                }
+        }
+        return 0;
+}
