e4b8b65905a33da76785e5e58542e7c30a39e2d7
fierceoj <61847904+fierceoj@users.noreply.github.com>
2020-05-03 12:39:05 -0500

    Update bye_pi.sh

--- bye_pi.sh
+++ bye_pi.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+#bash script to shut down Raspberry Pi when a button is pressed
+#must install wiringpi to run 'gpio' commands if using lite raspbian os (already installed on regular raspbian)
+
+#export BCM pin 21 and set as input
+#same pin as wiringPi pin 29 and physical pin 40
+#'gpio export 21 in' would also work here 
+echo 21 > /sys/class/gpio/export
+
+#ensure direction is in and initial pin state is set to HIGH
+gpio -g mode 21 in
+gpio -g mode 21 up
+
+while true
+do
+#could also read pin state with 'gpio -g read 21'
+        if [ `cat /sys/class/gpio/gpio21/value` == "0" ]; then
+                echo "Powering down the raspberry pi..."
+                sudo poweroff
+        fi
+done
