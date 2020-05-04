e7f655d4ca2630644e9657e2c24238035a765684
fierceoj <61847904+fierceoj@users.noreply.github.com>
2020-05-04 12:09:56 -0500

    Update bye_pi.sh

--- bye_pi.sh
+++ bye_pi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#bash script to shut down Raspberry Pi when a button is pressed
+#UNIX shell script to shut down Raspberry Pi when a button is pressed
 #must install wiringpi to run 'gpio' commands if using lite raspbian os (already installed on regular raspbian)
 
 #export BCM pin 21 and set as input
@@ -7,7 +7,7 @@
 #'gpio export 21 in' would also work here 
 echo 21 > /sys/class/gpio/export
 
-#ensure direction is in and initial pin state is set to HIGH
+#ensure direction is in and initial pin state is set to HIGH (1) by setting internal pull-up resistor
 gpio -g mode 21 in
 gpio -g mode 21 up
 
